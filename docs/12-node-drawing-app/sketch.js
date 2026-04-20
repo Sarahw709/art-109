ae87e8229897b425d153ce4bbb2ef42d793d0c55
GitHub Action <action@github.com>
2026-04-20 22:41:29 +0000

    Auto-sync portfolio files to docs folder

--- docs/12-node-drawing-app/sketch.js
+++ docs/12-node-drawing-app/sketch.js
@@ -1,15 +1,11 @@
-// ITP Networked Media, Fall 2014
-// https://github.com/shiffman/itp-networked-media
-// Daniel Shiffman
-// Video: https://www.youtube.com/watch?v=i6eP1Lw4gZk
+
 
 var socket;
 
 function setup() {
   createCanvas(400, 400).parent('sketch-holder');
   background(0);
-  // Same as the video's io.connect('http://localhost:3000') when you open this site from that server.
-  // Using io() follows whatever host/port you used in the browser (e.g. PORT=3456).
+  
   socket = io();
   socket.on('mouse', function (data) {
     console.log('Got: ' + data.x + ' ' + data.y);
