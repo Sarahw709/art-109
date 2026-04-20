26d71e1cb3308942ce0ba08f1022b5e767959ec5
Sarahw709 <sarah.wheeler01@sjsu.edu>
2026-04-20 15:36:53 -0700

    frfegvbw

--- Tutorials/12-node-drawing-app/public/server.js
+++ Tutorials/12-node-drawing-app/public/server.js
@@ -1,9 +1,36 @@
+// Based off of Shawn Van Every's Live Web
+// http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
+// Matches Coding Train: https://github.com/CodingTrain/sockets-and-p5
 
 var express = require('express');
-
 var app = express();
-var server = app.listen(3000);
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port, listen);
+server.on('error', function (err) {
+  console.error('Server error:', err.message);
+});
+
+function listen() {
+  var addr = server.address();
+  if (!addr) return;
+  console.log('Example app listening at http://' + addr.address + ':' + addr.port);
+}
 
 app.use(express.static('public'));
 
-console.log('Server running at http://localhost:3000');
+var { Server } = require('socket.io');
+var io = new Server(server);
+
+io.on('connection', function (socket) {
+  console.log('We have a new client: ' + socket.id);
+
+  socket.on('mouse', function (data) {
+    console.log("Received: 'mouse' " + data.x + ' ' + data.y);
+    socket.broadcast.emit('mouse', data);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('Client has disconnected');
+  });
+});
--- Tutorials/12-node-drawing-app/public/sketch.js
+++ Tutorials/12-node-drawing-app/public/sketch.js
@@ -1,9 +1,38 @@
+// ITP Networked Media, Fall 2014
+// https://github.com/shiffman/itp-networked-media
+// Daniel Shiffman
+// Video: https://www.youtube.com/watch?v=i6eP1Lw4gZk
+
+var socket;
+
 function setup() {
-    // Put the canvas inside <main> so CSS that targets main / main canvas applies
-    createCanvas(200, 200).parent('sketch-holder');
+  createCanvas(400, 400).parent('sketch-holder');
+  background(0);
+  // Same as the video's io.connect('http://localhost:3000') when you open this site from that server.
+  // Using io() follows whatever host/port you used in the browser (e.g. PORT=3456).
+  socket = io();
+  socket.on('mouse', function (data) {
+    console.log('Got: ' + data.x + ' ' + data.y);
+    fill(0, 0, 255);
+    noStroke();
+    ellipse(data.x, data.y, 20, 20);
+  });
 }
 
-function draw() {
-    background(51);
-    ellipse(mouseX, mouseY, 60, 60);
+function draw() {}
+
+function mouseDragged() {
+  fill(255);
+  noStroke();
+  ellipse(mouseX, mouseY, 20, 20);
+  sendmouse(mouseX, mouseY);
+}
+
+function sendmouse(xpos, ypos) {
+  console.log('sendmouse: ' + xpos + ' ' + ypos);
+  var data = {
+    x: xpos,
+    y: ypos,
+  };
+  socket.emit('mouse', data);
 }
